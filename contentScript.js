80ff0a95f4bc0ede241571f3899913ec45aab441
masal-98 <100738621+masal-98@users.noreply.github.com>
2023-09-05 18:49:15 +0200

    Add files via upload

--- contentScript.js
+++ contentScript.js
@@ -1,5 +1,7 @@
 var storage = chrome.storage.local;
+//storage.set({ selectedSpeed: 1});
 
+// Add event listener to the document to listen for message from poput telling contentScript that popup has been updated (speed has been chosen)
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
       if(request.method == "changeSpeed"){
@@ -10,8 +12,41 @@ chrome.runtime.onMessage.addListener(
       };
   } 
 );
-        
+
+// Add event listener to the document to listen for keydown events
+window.addEventListener('keydown', vidCtrl);
+
+////////////////////////////////////////////////////////functions
+//f to control playback rate
 function setVideoSpeed(selectedSpeed) {
   document.getElementsByTagName("video")[0].playbackRate= selectedSpeed;
   sendResponse({text: document.body.innerText, method: "changeSpeed"}); //same as innerText
 };
+
+//f to control arrows
+function vidCtrl(e) {
+  const vid = document.querySelector('video');
+  const key = e.code;
+
+  if (key === 'ArrowLeft') {
+    chrome.storage.sync.get(['selectedSpeed'], function(result) {
+      const selectedSpeed = result.selectedSpeed;
+      vid.currentTime = vid.currentTime+5-5*selectedSpeed; //if you dont put +-5 as an absolute term then the n seconds up or down are just gonna get added to the default +-5 seconds of youtube rewind when pressing the arrow keys
+    
+    if (vid.currentTime < 0) {
+      vid.pause();
+      vid.currentTime = 0;
+    }
+  });
+  } else if (key === 'ArrowRight') {
+    chrome.storage.sync.get(['selectedSpeed'], function(result) {
+      const selectedSpeed = result.selectedSpeed;
+      vid.currentTime = vid.currentTime-5+5*selectedSpeed; 
+
+      /*if (vid.currentTime > vid.duration) {
+      vid.pause();
+      vid.currentTime = vid.duration;
+      }*/
+    });
+  } 
+}
--- popup.js
+++ popup.js
@@ -2,15 +2,31 @@ var storage = chrome.storage.local;
 
 document.addEventListener('DOMContentLoaded', function() {
     const speedButtons = document.querySelectorAll('[data-speed]'); // Select all elements with data-speed attribute
-    speedButtons.forEach(button => {
+
+    // Load the last selected button index from storage and set the active state
+    storage.get(['lastSelectedIndex', 'selectedSpeed'], function(result) {
+        const lastIndex = result.lastSelectedIndex; // Default to 3 if not found (3 is speed 1)
+        const velocita = result.selectedSpeed || 1; // Default to 1x if not found
+        if (lastIndex >= 0 && lastIndex < speedButtons.length) { //this is basically just checking if lastIndex is a valid number between the possible ones (0 to 8)
+            speedButtons[lastIndex].classList.add('active');
+        }
+        chrome.storage.sync.set({ selectedSpeed: velocita });
+        chrome.storage.sync.set({ lastSelectedIndex: lastIndex });
+    });
+
+    speedButtons.forEach((button, index) => {
         button.addEventListener('click', function () {
             //when a button is clicked, the active class is added to the clicked button, giving it the active state appearance. The active class is removed from all other buttons to ensure only one button is in the active state at a time.
-            //speedButtons.forEach(btn => btn.classList.remove('active'));
-            //this.classList.add('active');
+            speedButtons.forEach(btn => btn.classList.remove('active'));
+            this.classList.add('active');
 
             const speed = parseFloat(this.getAttribute('data-speed'));
-            //console.log(`Selected playback speed: ${speed}`);
+            
+            // Store the index and the speed of the clicked button in storage
             chrome.storage.sync.set({ selectedSpeed: speed });
+            chrome.storage.sync.set({lastSelectedIndex: index});
+
+            console.log(`Clicked on button at position ${index} with speed ${speed}x`);
 
             chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
                 chrome.tabs.sendMessage(tabs[0].id, {method: "changeSpeed"});
